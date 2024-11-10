8298f26bb3406efd1c4cbc96bef726cea4db9f75
Sharath <nssharath123@gmail.com>
2024-11-10 12:22:57 +0530

    first commit

--- script.js
+++ script.js
@@ -1,8 +1,47 @@
-window.addEventListener('scroll', function () {
-    const navbar = document.getElementById('navbar');
-    if (window.scrollY > 50) {
-        navbar.classList.add('scrolled');
-    } else {
-        navbar.classList.remove('scrolled');
+document.addEventListener("DOMContentLoaded", function () {
+    const toggleButtons = document.querySelectorAll(".faq li span.toggle-btn");
+    toggleButtons.forEach(button => {
+        button.addEventListener("click", function () {
+            const answer = this.nextElementSibling;
+
+            if (answer.style.display === "block") {
+                answer.style.display = "none";
+                this.textContent = "Toggle";
+            } else {
+                answer.style.display = "block";
+                this.textContent = "Close";
+            }
+        });
+    });
+
+
+    let slideIndex = 0;
+    const slides = document.querySelectorAll(".product-card img");
+    const totalSlides = slides.length;
+
+    function showSlide(index) {
+
+        slides.forEach((slide, i) => {
+            slide.style.display = "none";
+        });
+
+
+        slides[index].style.display = "block";
     }
+
+    function nextSlide() {
+        slideIndex = (slideIndex + 1) % totalSlides;
+        showSlide(slideIndex);
+    }
+
+    function prevSlide() {
+        slideIndex = (slideIndex - 1 + totalSlides) % totalSlides;
+        showSlide(slideIndex);
+    }
+
+    showSlide(slideIndex);
+    setInterval(nextSlide, 3000);
+    document.querySelector("#next-slide").addEventListener("click", nextSlide);
+    document.querySelector("#prev-slide").addEventListener("click", prevSlide);
 });
+
